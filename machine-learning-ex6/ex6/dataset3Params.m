9624a19329be0fbe44d67a44c38c4811e2ad38d5
Chuang Yu Min <chg.yumin@gmail.com>
2018-07-31 17:43:35 -0300

    Ex6 completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%% Code used to get the best combination of C and Sigma
+%% Got C = 1.00 and Sigma = 0.10
+%{
+cVec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmaVec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+error = 1;
 
+for c=cVec
+  for s=sigmaVec
+    fprintf('Running for C: %.2f, Sigma: %.2f', c, s);
+    model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+    p = svmPredict(model, Xval);
+    new_error = mean(double(p ~= yval));
+    if new_error < error
+      error = new_error;
+      C = c;
+      sigma = s;
+  end
+end
 
-
-
-
-
+fprintf('Best C, sigma: %.2f, %.2f', C, sigma);
+%}
+C = 1.00;
+sigma = 0.10;
 % =========================================================================
 
 end
